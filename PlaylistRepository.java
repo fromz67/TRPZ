269646f6832662374e487d6ae44fd6820fd26a3c
fromz67 <144937756+fromz67@users.noreply.github.com>
2025-10-28 23:27:49 +0200

    Delete PlaylistRepository.java

--- PlaylistRepository.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface PlaylistRepository extends JpaRepository<Playlist, Integer>
-{
- // Spring сам реалізує всі базові CRUD-операції
-}
