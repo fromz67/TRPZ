b7c79e6abc045718b475d45069fff09b5f38e6fd
fromz67 <144937756+fromz67@users.noreply.github.com>
2025-10-28 23:36:14 +0200

    Create AudioPlayerApiApplication.java

---
+++ lab3/AudioPlayerApiApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioPlayerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioPlayerApiApplication.class, args);
+    }
+}
