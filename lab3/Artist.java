39e9effa11e7dd4f87737b73f2766c065421fac9
fromz67 <144937756+fromz67@users.noreply.github.com>
2025-10-28 23:35:09 +0200

    Create Artist.java

---
+++ lab3/Artist.java
@@ -0,0 +1,24 @@
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "Artists")
+public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Album> albums;
+
+    public Artist() {}
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Set<Album> getAlbums() { return albums; }
+    public void setAlbums(Set<Album> albums) { this.albums = albums; }
+}
