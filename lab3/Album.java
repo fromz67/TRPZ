e4b92019b85d69867c9aa7a2e3a0f1285745b595
fromz67 <144937756+fromz67@users.noreply.github.com>
2025-10-28 23:35:27 +0200

    Create Album.java

---
+++ lab3/Album.java
@@ -0,0 +1,34 @@
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "Albums")
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private int year;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ArtistId")
+    private Artist artist;
+
+    @OneToMany(mappedBy = "album", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Track> tracks;
+
+    public Album() {}
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+    public Artist getArtist() { return artist; }
+    public void setArtist(Artist artist) { this.artist = artist; }
+    public Set<Track> getTracks() { return tracks; }
+    public void setTracks(Set<Track> tracks) { this.tracks = tracks; }
+}
