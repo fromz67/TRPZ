1571a14bd356820f743bc5fb5dd37ea011fe1b21
fromz67 <144937756+fromz67@users.noreply.github.com>
2025-10-28 23:23:20 +0200

    Create PlaylistController.java

---
+++ lab3/PlaylistController.java
@@ -0,0 +1,36 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping("/api/playlists")
+public class PlaylistController {
+
+    @Autowired
+    private PlaylistService playlistService;
+
+    public static class CreatePlaylistRequest {
+        public String name;
+    }
+
+    @PostMapping
+    public ResponseEntity<Playlist> createPlaylist(@RequestBody CreatePlaylistRequest request) {
+        Playlist newPlaylist = playlistService.createPlaylist(request.name);
+        return new ResponseEntity<>(newPlaylist, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{playlistId}/tracks")
+    public ResponseEntity<Playlist> addTrack(
+            @PathVariable int playlistId, 
+            @RequestBody int trackId) {
+        Playlist updatedPlaylist = playlistService.addTrackToPlaylist(playlistId, trackId);
+        return ResponseEntity.ok(updatedPlaylist);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Playlist>> getAllPlaylists() {
+        List<Playlist> playlists = playlistService.getAllPlaylists();
+        return ResponseEntity.ok(playlists);
+    }
+}
