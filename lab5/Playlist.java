34c1922299efea66eb73be02c3e47176b4ee057a
fromz67 <144937756+fromz67@users.noreply.github.com>
2025-11-26 00:06:38 +0200

    Playlist.java

---
+++ lab5/Playlist.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist {
+    private final String name;
+    private final List<Track> tracks = new ArrayList<>();
+
+    public Playlist(String name) {
+        this.name = name;
+    }
+
+    public void addTrack(Track track) {
+        tracks.add(track);
+        System.out.println("Трек \"" + track.getTitle() +
+                "\" додано до плейлиста \"" + name + "\"");
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+}
