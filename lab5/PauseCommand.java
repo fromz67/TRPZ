8da5259756392fa6613e7cdaa92750e3be7192e3
fromz67 <144937756+fromz67@users.noreply.github.com>
2025-11-26 00:05:27 +0200

    Create PauseCommand.java

---
+++ lab5/PauseCommand.java
@@ -0,0 +1,14 @@
+public class PauseCommand implements Command {
+
+    private final AudioEngine engine;
+
+    public PauseCommand(AudioEngine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Команда: поставити на паузу");
+        engine.pause();
+    }
+}
