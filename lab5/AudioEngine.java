568014983b09f8633f340aecd9faa17643610ddb
fromz67 <144937756+fromz67@users.noreply.github.com>
2025-11-26 00:06:23 +0200

    AudioEngine.java

---
+++ lab5/AudioEngine.java
@@ -0,0 +1,10 @@
+public class AudioEngine {
+
+    public void play(String filePath) {
+        System.out.println("Відтворення файлу: " + filePath);
+    }
+
+    public void pause() {
+        System.out.println("Відтворення поставлено на паузу");
+    }
+}
