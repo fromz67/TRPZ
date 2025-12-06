b53b5a8fc947d2bf829cb08410ea1f66fc5f6ea2
fromz67 <144937756+fromz67@users.noreply.github.com>
2025-12-06 14:27:23 +0200

    1.0

---
+++ lab8/MusicComponent.java
@@ -0,0 +1,13 @@
+public abstract class MusicComponent {
+    public void play() {
+        throw new UnsupportedOperationException("Операція не підтримується");
+    }
+
+    public void add(MusicComponent component) {
+        throw new UnsupportedOperationException("Операція не підтримується");
+    }
+
+    public void remove(MusicComponent component) {
+        throw new UnsupportedOperationException("Операція не підтримується");
+    }
+}
